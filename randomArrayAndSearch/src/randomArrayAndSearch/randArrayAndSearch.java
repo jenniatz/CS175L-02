2908f00250f877cf3e4a73bc4c0ae9cf6934136a
12019 <12019@192.168.1.164>
2021-04-14 23:46:53 -0400

    This is my Random Array Assignment

---
+++ randomArrayAndSearch/src/randomArrayAndSearch/randArrayAndSearch.java
@@ -0,0 +1,54 @@
+package randomArrayAndSearch;
+
+import java.util.Random;
+
+public class randArrayAndSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		Random list = new Random();
+		
+		int numbers;
+		
+		
+		
+		
+		/*
+		 * int lowestVal = Integer.MAX_VALUE; int highestVal = Integer.MIN_VALUE;
+		 */
+		
+		for(int item = 1; item <=50; item++)
+		{
+			 numbers = 1+list.nextInt(25);
+			
+			System.out.println(numbers + "  " + " \n");
+			
+			
+			
+			
+				
+			
+			
+		}
+			
+		
+			
+		
+		
+		
+		}
+		 
+				
+		
+					
+				
+}
+						
+		
+			
+			
+		
+	
+
